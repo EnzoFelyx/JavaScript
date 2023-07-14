8bdc36578b1966788da5653183af561960c9ce66
EnzoFelyx <enzofelyx@gmail.com>
2023-07-14 12:28:39 -0300

    media

--- Arrays/Introduction_Array.js
+++ Arrays/Introduction_Array.js
@@ -1,1 +1,7 @@
-testando
+//Calculando média de alunos
+
+const notas = [10, 9, 6, 4, 10];
+
+const media = (notas[0] + notas[1] + notas [2] + notas[3] + notas[4]) / notas.length;
+
+console.log(media);
