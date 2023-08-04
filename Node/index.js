f6408261be7f1d0aca8b0ff5fcb006253cf7959a
EnzoFelyx <enzofelyx@gmail.com>
2023-08-03 23:25:57 -0300

    regex

--- Node/index.js
+++ Node/index.js
@@ -4,12 +4,15 @@
 import chalk from "chalk"; //forma atual "import"
 import fs from 'fs';
 
-function fixError (badnews) {
+function fixError(badnews) {
     console.log(badnews);
     throw new Error(chalk.red(badnews.code, 'There is no file in this directory'));
 }
 
-function takeFile(fileAdress) {
+
+//---------------------------------------------------------------------------------//
+
+/* function takeFile(fileAdress) { //synchronous
     const encoding = 'utf-8';
     fs.readFile(fileAdress, encoding, (badnews, texto) => {
         if(badnews) {
@@ -17,8 +20,51 @@ function takeFile(fileAdress) {
         }
         console.log(chalk.green(texto))
     })
-}
+} */
+
 
 //takeFile('./arquivos/texto.md');
-takeFile('../../../test.txt');
+//takeFile('../../../test.txt');
+
+
+//---------------------------------------------------------------------------------//
+
+/* function newTakeFile(fileAdress) { //asynchronous
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(fileAdress, encoding)
+        .then((text) => console.log(chalk.green(text)))
+        .catch(fixError)
+}
+
+newTakeFile('./arquivos/text.md') */
+
+// synchronous = read line by line (bad because it stops program until finish an function, in this case, read all the file)
+// asynchronous = multiple thinks at same time (way better, because we never know how big will be an file)
+
+
+//---------------------------------------------------------------------------------//
+
+// async/await
+
+async function finalTakeFile(fileAdress) {
+    try {
+        const encoding = 'utf-8';
+        const text = await fs.promises.readFile(fileAdress, encoding)
+        console.log(extractLinks(text));
+    } catch (badnews) {
+        fixError(badnews)
+    }
+}
+
+finalTakeFile('./arquivos/texto.md');
+
+
+function extractLinks(text) {
+
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capture = [...text.matchAll(regex)];
+    const results = capture.map(capture => ({ [capture[1]]: [capture[2]] }))
+    return results;
+}
 
