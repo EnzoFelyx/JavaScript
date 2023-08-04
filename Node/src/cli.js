5ad772307806e59f3b365f087b0ed644e84446c8
EnzoFelyx <enzofelyx@gmail.com>
2023-08-04 11:17:37 -0300

    handling errors

--- Node/src/cli.js
+++ Node/src/cli.js
@@ -4,23 +4,35 @@ import finalTakeFile from "./index.js";
 
 const adress = process.argv;
 
-function printList(result) {
-    console.log(chalk.yellow('Link lista'), result)
+function printList(result, fileName = '') {
+    console.log(
+        chalk.yellow(`Link list`),
+        chalk.black.bgGreen(fileName),
+        result);
 }
 
 async function TextProcess(arg) {
 
     const adress = arg[2];
 
+    try {
+        fs.lstatSync(adress);
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            console.log(chalk.red("File or directory doesn't exist"));
+            return;
+        }
+    }
+
     if (fs.lstatSync(adress).isFile()) {
         const result = await finalTakeFile(arg[2])
-        printList(result)
+        printList(result,)
     }
     else if (fs.lstatSync(adress).isDirectory()) {
         const files = await fs.promises.readdir(adress)
         files.forEach(async (fileName) => {
             const list = await finalTakeFile(`${adress}/${fileName}`)
-            printList(list);
+            printList(list, fileName);
         })
     }
 }
