0d54893e873424cf0fe1a2641af44e5f2054dd92
EnzoFelyx <enzofelyx@gmail.com>
2023-08-04 11:00:18 -0300

    using dir

--- Node/src/cli.js
+++ Node/src/cli.js
@@ -1,11 +1,28 @@
 import chalk from "chalk";
+import fs from 'fs';
 import finalTakeFile from "./index.js";
 
 const adress = process.argv;
 
-async function TextProcess(adress) {
-    const result = await finalTakeFile(adress[2])
-    console.log(chalk.yellow('link list'), result);
+function printList(result) {
+    console.log(chalk.yellow('Link lista'), result)
+}
+
+async function TextProcess(arg) {
+
+    const adress = arg[2];
+
+    if (fs.lstatSync(adress).isFile()) {
+        const result = await finalTakeFile(arg[2])
+        printList(result)
+    }
+    else if (fs.lstatSync(adress).isDirectory()) {
+        const files = await fs.promises.readdir(adress)
+        files.forEach(async (fileName) => {
+            const list = await finalTakeFile(`${adress}/${fileName}`)
+            printList(list);
+        })
+    }
 }
 
 TextProcess(adress);
